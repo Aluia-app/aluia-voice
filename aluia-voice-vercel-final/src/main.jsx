d28ac379c933f517cc44cf5ac59e95419422acba
Aluia-app <nicola.zaccarini10@gmail.com>
2025-07-01 16:21:14 +0100

    Delete aluia-voice-vercel-final directory

--- aluia-voice-vercel-final/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VoiceDemo from './VoiceDemo'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VoiceDemo />
-  </React.StrictMode>
-)
