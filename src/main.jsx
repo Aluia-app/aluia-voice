eb024302db0f1b1de08e4a4c9ac5c510e71c3281
Aluia-app <nicola.zaccarini10@gmail.com>
2025-07-02 19:06:33 +0100

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import VoiceDemo from './VoiceDemo.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <VoiceDemo />
-  </React.StrictMode>
-)
